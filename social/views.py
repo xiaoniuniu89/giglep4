152ad3241b7f5ce15037900787c83383738d3234
Daniel Callaghan <dan_cal89@hotmail.com>
2022-01-15 22:20:05 +0000

    add sign up view

--- social/views.py
+++ social/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserSignUpForm
+from django.contrib import messages
 
 def landing(request):
     return render(request, 'landing/landing.html')
+
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}, you can now login!')
+            return redirect('login')
+    else:
+        form = UserSignUpForm()
+       
+        
+    return render(request, 'landing/sign-up.html', {'form': form})
