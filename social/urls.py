140c62785e7b8ca8fe9433665c5d25f793874773
Daniel Callaghan <dan_cal89@hotmail.com>
2022-01-15 22:31:51 +0000

    add login url and template for html

--- social/urls.py
+++ social/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views as social_views
+from django.contrib.auth import views as auth_views
+from .forms import UserLoginForm
 
 urlpatterns = [
     path('', social_views.landing, name='landing-home'),
     path('sign-up/', social_views.sign_up, name='sign-up'),
+    path('login/', auth_views.LoginView.as_view(template_name='landing/login.html', authentication_form=UserLoginForm), name='login'),
 ]
