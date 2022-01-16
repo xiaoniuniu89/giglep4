2d69bbbde74da918404d7b2268728385d2dfdbc8
Daniel Callaghan <dan_cal89@hotmail.com>
2022-01-16 11:59:47 +0000

    add comment model and migrations

--- organiser/models.py
+++ organiser/models.py
@@ -15,3 +15,10 @@ class Post(models.Model):
     
     def __str__(self):
         return f'{self.author}\'s post'
+
+
+class Comment(models.Model):
+    comment = models.TextField(max_length=200)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
