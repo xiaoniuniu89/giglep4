cda914beab4e0571e7fb2e78b317316fe8d036ef
Daniel Callaghan <dan_cal89@hotmail.com>
2022-01-17 07:42:47 +0000

    update landing page links and styles

--- gigle/settings.py
+++ gigle/settings.py
@@ -54,8 +54,8 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/social/feed'
+# LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
