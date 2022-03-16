361a61bbd09f2a8c64e2d4fa5c4b77c4892bbc3a
Daniel Callaghan <dan_cal89@hotmail.com>
2022-03-16 07:03:05 +0000

    add comments and fix pep8 errors

--- social/models.py
+++ social/models.py
@@ -3,14 +3,20 @@ from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 from cloudinary.models import CloudinaryField
 
+
 # extends user model
 class Musician(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # delete profile for musician and not user account 
+    """
+    A musician is created through signals.py whenever
+    a user signs up for an account
+    """
+    # delete profile for musician and not user account
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = CloudinaryField('image', default='placeholder')
     instrument = models.CharField(default='', max_length=50)
     location = models.CharField(default='', max_length=50)
     blurb = models.TextField(default='', max_length=100)
-    
+
     # for admin page to display something other than musician object
     def __str__(self):
         return f'{self.user}\'s profile'
