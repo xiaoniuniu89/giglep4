1da68b90e6e26a86018d3a96743d09ca176cfbf8
Daniel Callaghan <dan_cal89@hotmail.com>
2022-03-16 07:26:41 +0000

    add comments and fix pep8 errors

--- social/urls.py
+++ social/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views as social_views
-from .forms import UserLoginForm, UserPasswordResetForm, PasswordChangeForm
+from .forms import (
+    UserLoginForm,
+    UserPasswordResetForm,
+    PasswordChangeForm
+    )
 
 
 # views for landing page
