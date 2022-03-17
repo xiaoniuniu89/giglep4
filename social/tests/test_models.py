1abbc04f071d66a25bdb745f2fd04b66e77db75c
Daniel Callaghan <dan_cal89@hotmail.com>
2022-03-17 10:27:00 +0000

    add comments and fix pep8 errors

--- social/tests/test_models.py
+++ social/tests/test_models.py
@@ -1,18 +1,19 @@
 from django.test import TestCase, Client
-from social.models import Musician
 from django.contrib.auth.models import User
 
 
 class TestModels(TestCase):
+    """test social app user model"""
 
     def setUp(self):
+        """set up test variables"""
         self.client = Client()
-        
         self.user1 = User.objects.create(
-            username = 'test',
-            email = 'test@email.com',
-            password = 'test12344321',
+            username='test',
+            email='test@email.com',
+            password='test12344321',
         )
 
     def test_user_has_profile(self):
+        """tests signal to create musician profile works"""
         self.assertEquals(self.user1.musician.user.username, 'test')
