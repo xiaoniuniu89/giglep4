2f5491fbffbc32a533009bbb2025696e12cacdb6
Daniel Callaghan <dan_cal89@hotmail.com>
2022-03-23 13:28:50 +0000

    add comments and remove jshint errors

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,35 @@
 // to open profile dropdown menu
 // organiser.css line 33 
 $('.profile-dropdown-icon').on('click', function() {
-    $('.profile-dropdown').toggleClass('profile-nav--open', 500),
-    $('.close-dropdown').toggleClass('hide')
-    $('.close-dropdown').toggleClass('move-forward')
-    $('.nav-list li:nth-child(2)').toggleClass('hide')
+    $('.profile-dropdown').toggleClass('profile-nav--open', 500);
+    $('.close-dropdown').toggleClass('hide');
+    $('.close-dropdown').toggleClass('move-forward');
+    $('.nav-list li:nth-child(2)').toggleClass('hide');
 
-})
+});
 
 // to open and close the notification menu
 $('.notifications-dropdown-icon').on('click', function() {
-    $('.notification-dropdown').toggleClass('notification-nav--open', 500)
-    $('.bell').toggleClass('hide')
-})
+    $('.notification-dropdown').toggleClass('notification-nav--open', 500);
+    $('.bell').toggleClass('hide');
+});
 
 // open and close modal to edit profile 
 
 $('#edit-profile').on('click', function(){
-    $('#profile-update-modal').css('display', 'block')
-})
+    $('#profile-update-modal').css('display', 'block');
+});
 
 $('#close-modal').on('click', function(){
-    $('#profile-update-modal').css('display', 'none')
-})
+    $('#profile-update-modal').css('display', 'none');
+});
 
-// for notifications 
+// for removing notifications 
 // Help from this tutorial
 // https://www.youtube.com/watch?v=_JKWYkz597c&t=4s
+
+// django documentation includes the following function to get csrf token 
+// https://docs.djangoproject.com/en/3.1/ref/csrf/ 
 
 function getCookie(name) {
     let cookieValue = null;
@@ -48,23 +51,21 @@ function getCookie(name) {
 // https://www.youtube.com/watch?v=_JKWYkz597c&t=4s
 
 function removeNotification(removeNotificationURL, redirectURL) {
+    // from onclick in remove notification in show_notifications.html
+    // get csrf token 
     const csrftoken = getCookie('csrftoken');
+    // make a new xml request 
     let xmlhttp = new XMLHttpRequest();
+    // if request is ready 
     xmlhttp.onreadystatechange = function() {
+        // if 200 response go ahead and redirect 
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {
             if (xmlhttp.status == 200) {
-                window.location.replace(redirectURL)
-            } else('There was an error');
+                window.location.replace(redirectURL);
+            } else return ('There was an error');
         }
-    } 
+    };
     xmlhttp.open("DELETE", removeNotificationURL, true);
     xmlhttp.setRequestHeader("X-CSRFToken", csrftoken);
     xmlhttp.send();
 }
-
-
-
-
-
-
-
